1a1d858f89c14ce6a65fa3a60c2f21aa929bd30e
Howard Tyson <howard@howardtyson.com>
2013-10-17 09:12:55 -0400

    Fleshing out server logic (stealing it mostly from Zivtech's auth proxy).

--- server.js
+++ server.js
@@ -16,3 +16,40 @@ console.log(config);
 
 var app = express();
 var proxy = new httpProxy.RoutingProxy();
+
+// configure Express
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.logger());
+  app.use(express.cookieParser());
+  app.use(express.methodOverride());
+  app.use(express.session({ key: 'sid', cookie: { domain: config.cookieDomain }, secret: config.sessionSecret }));
+  // Initialize Passport!  Also use passport.session() middleware, to support
+  // persistent login sessions (recommended).
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user, message: req.flash('error') });
+});
+
+// We add the express bodyparser middleware here so that we don't
+// intercept posts in any other context.
+app.post('/login', express.bodyParser(),
+  passport.authenticate('local', { failureRedirect: '/proxy-login', failureFlash: true }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+app.get('/proxy-logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+server = https.createServer(options, app);
+server.listen(config.port);

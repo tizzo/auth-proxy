f566afbc8ec6d72e6b0e206a51f9aae5b395c19f
Howard Tyson <howard@howardtyson.com>
2013-10-22 22:46:31 -0700

    Removing debug server.

--- server.js
+++ server.js
@@ -152,14 +152,6 @@ function ensureAuthenticated(req, res, next) {
   res.redirect('/login');
 }
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.write('request successfully proxied: ' + req.url +'\n' + JSON.stringify(req.headers, true, 2));
-        res.end();
-}).listen(9000); 
-
-var proxy = new httpProxy.RoutingProxy();
-
 // Accepts a req, looks up the route.
 function lookupRoute(req) {
   var possibleMatches = [];

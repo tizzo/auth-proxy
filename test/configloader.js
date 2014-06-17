aa5e140f259600f632618ba368a1bc64fb80b26c
Howard Tyson <howard@howardtyson.com>
2014-06-17 18:27:55 -0400

    Refactoring config loader to prep for conf and route directories.

--- test/configloader.js
+++ test/configloader.js
@@ -4,13 +4,25 @@ var app = require('../index');
 
 var ConfigLoader = app.ConfigLoader;
 
+var configDir = path.resolve(path.join('.', 'test', 'fixtures', 'ConfigLoader', 'json-objects'));
+
 describe('ConfigLoader', function() {
-  describe('loadDir', function() {
-    it('should combine config objects', function(done) {
-      var configDir = path.resolve(path.join('.', 'test', 'fixtures', 'ConfigLoader', 'json-objects'));
+  describe('loadDirAsArray', function() {
+    it('should load an array of config objects', function(done) {
       ConfigLoader.logErrors = false;
-      ConfigLoader.loadDir(configDir, function(error, conf) {
+      ConfigLoader.loadDirAsArray(configDir, function(error, conf) {
         should.exist(conf);
+        conf.should.be.instanceof(Array).and.have.lengthOf(2);
+        conf[0].foo.should.equal('bar');
+        conf[1].bar.should.equal('bot');
+        conf[1].plink.should.equal('plunk');
+        done(error);
+      });
+    });
+  });
+  describe('loadDirAsMergedObject', function() {
+    it('should load an array of config objects', function(done) {
+      ConfigLoader.loadDirAsMergedObject(configDir, function(error, conf) {
         conf.foo.should.equal('bar');
         conf.bar.should.equal('bot');
         conf.plink.should.equal('plunk');

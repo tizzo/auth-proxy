c71df0fb1ff302dc81468e126375ed53a414012f
Dima Sumaroka <dmitriy.sumaroka@gmail.com>
2014-09-10 00:26:49 +0000

    Removed Keys and Secret as well as changed the domain name in the config

--- examples/config/DrupalOAuth.js
+++ examples/config/DrupalOAuth.js
@@ -1,10 +1,10 @@
 authenticationStrategies:
   DrupalOAuth:
-    providerURL: "http://extranet.local/"
-    consumerKey: "AustmLogeVEGKkgHGovPw2iK2GgSduQg"
-    consumerSecret: "4qypvmzRF4CuLr3nVG7vYuEg575W9xfJ"
-    requestTokenURL: "http://extranet.local/oauth/request_token"
-    accessTokenURL: "http://extranet.local/oauth/access_token"
-    userAuthorizationURL: "http://extranet.local/oauth/authorize"
-    resourceURL: "http://extranet.local/rest/system/connect/user/info"
-    resourceEndpoint: "rest/system/connect"
+    providerURL: "http://yourdruplasite.com/"
+    consumerKey: "YOUR_KEY"
+    consumerSecret: "YOUR_SECRET"
+    requestTokenURL: "http://yourdruplasite.com/oauth/request_token"
+    accessTokenURL: "http://yourdruplasite.com/oauth/access_token"
+    userAuthorizationURL: "http://yourdruplasite/oauth/authorize"
+    resourceURL: "http://yourdruplasite/rest/system/connect/user/info"
+    resourceEndpoint: "rest/system/connect" // <-- thats the default

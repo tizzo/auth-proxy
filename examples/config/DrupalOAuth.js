2895748ff473829a1cf638ed9c1bf2062305ee5c
Dima Sumaroka <dmitriy.sumaroka@gmail.com>
2014-09-10 00:21:34 +0000

    Added and tested Drupal OAuth Stragety, added example config file

---
+++ examples/config/DrupalOAuth.js
@@ -0,0 +1,10 @@
+authenticationStrategies:
+  DrupalOAuth:
+    providerURL: "http://extranet.local/"
+    consumerKey: "AustmLogeVEGKkgHGovPw2iK2GgSduQg"
+    consumerSecret: "4qypvmzRF4CuLr3nVG7vYuEg575W9xfJ"
+    requestTokenURL: "http://extranet.local/oauth/request_token"
+    accessTokenURL: "http://extranet.local/oauth/access_token"
+    userAuthorizationURL: "http://extranet.local/oauth/authorize"
+    resourceURL: "http://extranet.local/rest/system/connect/user/info"
+    resourceEndpoint: "rest/system/connect"

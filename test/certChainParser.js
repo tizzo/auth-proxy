4f277e964b318a1ce2306c847214834d1e4de5db
Howard Tyson <howard@howardtyson.com>
2014-10-14 22:27:59 -0400

    Adding certificate chain bundle parsing.

---
+++ test/certChainParser.js
@@ -0,0 +1,14 @@
+var should = require('should');
+var fs = require('fs');
+var path = require('path');
+var chainParser = require('../lib/certChainParser');
+
+describe('certificateChainParser', function() {
+  it('should parse a valid certificate chain', function(done) {
+    fs.readFile(path.resolve(path.join('test', 'ssl', 'testChain.pem')), 'utf8', function(error, file) {
+      var ca = chainParser(file);
+      ca.length.should.equal(4);
+      done();
+    });
+  });
+});
